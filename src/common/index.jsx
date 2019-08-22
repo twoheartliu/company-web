d1190a54473a015577cd7b5b7fa55fc4191bbcba
twoheart <twoheart@vip.qq.com>
2019-08-22 10:29:17 +0800

    some remake

--- src/common/index.jsx
+++ src/common/index.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 import "./style.css";
-import imgLogo from "../statics/logo1.png";
+import imgLogo from "../statics/logo2.png";
 
 const Header = () => {
   return (
     <div className="header-wrapper">
-      <div className="header-box clearfix">
-        <div className="logo">
-          <a href="/">
-            <img src={imgLogo} alt="logo" />
-          </a>
-        </div>
-        <nav className="nav-menu clearfix">
-          <div className="nav-left">
-            <a href="">首页</a>
-          </div>
-          <div className="nav-left">
-            <a href="">解决方案</a>
-          </div>
-          <div className="nav-left">
-            <a href="">产品服务</a>
-          </div>
-          <div className="nav-left">
-            <a href="">关于链平方</a>
-          </div>
-        </nav>
+      <div className="logo-wrapper">
+        <img src={imgLogo} alt="logo" />
       </div>
+      <nav className="nav-wrapper">
+        <ul>
+          <li>首页</li>
+          <li>导航1</li>
+          <li>导航2</li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Sliders from "./sliders";
+// import Sliders from "./sliders";
 import "./style.css";
 const Home = () => {
   return (
     <div className="home">
-      <Sliders />
+      {/* <Sliders /> */}
       Home
     </div>
   );
